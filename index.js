9929f57d42e6430aa17237d7a56cdb832f818b24
soonhmahar <maharsoonh834@gmail.com>
2024-05-30 22:32:14 -0700

    2 commit

---
+++ index.js
@@ -0,0 +1,71 @@
+// let person1 = {
+
+//      name : " wasif",
+//      cnic : 87563437674,
+//      language : " sindhi ",
+//      class : 'javascipt' ,
+//       speck : function (stuents){ 
+//         console.log( stuents + this.name );
+
+//       }
+// }
+// let person2 = {
+
+//     name : " abdullah", //property
+//     cnic : 87563437674, // key 
+//     language : " sindhi ",
+//     class : 'javascipt' ,
+//      speck : function (stuents){ 
+//        console.log( stuents + this.name );
+
+//      }
+// }
+//  person1.speck(" hello i m  ");
+//  person2.speck(" hello i m  ")
+
+
+
+// let person1 = {
+
+//      name : " wasif",
+//      cnic : 87563437674,
+//      language : " sindhi ",
+//      class : 'javascipt' ,
+//       speck : function (stuents){ 
+//         console.log( stuents + this.name );
+
+//       }
+// }
+// let person2 = {
+
+//     name : " abdullah", //property
+//     cnic : 87563437674, // key 
+//     language : " sindhi ",
+//     class : 'javascipt' ,
+//      speck : function (stuents){ 
+//        console.log( stuents + this.name );
+
+//      }
+//     }
+
+//     let students = [
+//         person1 , person2
+//     ];
+//     console.log(students);
+
+
+//        function students (){
+//        return {
+
+//          name : prompt("inter your name "),
+//          cnic : +prompt("inter your card num "), 
+//          language : prompt("inter your language "), 
+//          class : prompt("inter your class")
+
+
+//              }
+// }
+
+
+//  let student1 = [students( )];
+//  console.log(student1);
